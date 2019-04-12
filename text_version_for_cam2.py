c28f88c62a16eaa3a49afda76065697620c3bd82
Raya Litova <raya.litova23@gmail.com>
2019-04-12 21:52:20 +0300

    ne raboti proverkata

--- text_version_for_cam2.py
+++ text_version_for_cam2.py
@@ -9,12 +9,16 @@ import time
 
 
 
-    
+pp1=0
+pp2=0
+
 wins=[0,0]
 lower = {'blue':(97, 100, 117)} 
 upper = {'blue':(117,255,255)}
 colors = {'blue':(255,0,0),
-          'red':(0,0,255)}
+          'red':(0,0,255),
+          'pink':(153,51,255),
+          'purple':(166,26,155)}
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 first_player_side = (350, 30)
@@ -30,11 +34,6 @@ number_dots = 0
 draw_history=[{},{}]
 cam=cv2.VideoCapture(0)
 
-    
-
-
-    
-   
 while True:
     rval, frame = cam.read()
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
@@ -48,41 +47,48 @@ while True:
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)[-2]
         center = None
-                
+
+         
         if len(cnts) > 0:
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
                 
             if radius > 25: 
                 number_dots += 1
                 draw_history[pl][number_dots] = x, y
-                #print('x:', x, 'y:', y)
-            
+                print('x:', x, 'y:', y)
+                #ne raboti
+                print(((draw_history[pl][number_dots][0]<=555 and draw_history[pl][number_dots][0]>=545) or (draw_history[pl][number_dots][0]<=260 and draw_history[pl][number_dots][0]>=245)) and ((draw_history[pl][number_dots][1]<=255 and draw_history[pl][number_dots][1]>=240) or (draw_history[pl][number_dots][1]<=80 and draw_history[pl][number_dots][1]>=65)))
+                #    
+
+
+                
+    cv2.rectangle(frame, (250,70), (550,250), (255,255,255), 5)
+    
     for item in draw_history[pl]:
-        cv2.circle(frame, (int(draw_history[pl][item][0]), int(draw_history[pl][item][1])), 1, colors["red"], 2)
+        cv2.circle(frame, (int(draw_history[pl][item][0]), int(draw_history[pl][item][1])), 3, colors["red"], 6)
 
 
     alpha=0.5
-    cv2.rectangle(frame, (250,70), (550,250), (255,255,255), 5)
-    #cv2.line(frame, (330,50), (330,450), (0,0,255), 5)
     cv2.addWeighted(frame, alpha, frame, 0.5, 0, frame)
     frame = cv2.flip(frame, 1)
+
 
     if not is_game_started:
         draw_history[pl]= {}
         cv2.putText(frame, 'Type "s" to start the game', second_player_side, font, fontScale, fontColor, lineType)
-
     if is_game_started:
         cv2.putText(frame, 'The game is started', second_player_side, font, fontScale, fontColor, lineType)
 
+
     keys_shortcut = cv2.waitKey(1) & 0xFF
     if keys_shortcut == ord("s"):
-
         is_game_started = True
-            
 
+        
     cv2.imshow('Player', frame)
 
     if cv2.waitKey(1)==27:
@@ -107,36 +113,60 @@ while True:
             cv2.destroyAllWindows()
             img = np.zeros((800,800,3), np.uint8)
             img = cv2.rectangle(img, (250,70), (550,250), (255,255,255),5)
-            for i in range(2):
-                if i==0:
-                    c='red'
-                elif i==1:
-                    c='blue'
-                for item in draw_history[i]:
-                    img = cv2.circle(img, (int(draw_history[i][item][0]), int(draw_history[i][item][1])), 1, colors[c], 2)
 
-            
+           
+            for i in range(2):
+                for item in draw_history[i]:
+                   #print((draw_history[i][item][1]<=255 and draw_history[i][item][1]>=240) or (draw_history[i][item][1]<=80 and draw_history[i][item][1]>=65))
+                    
+                    #ne raboti proverkata
+                    if (draw_history[i][item][0]<=555 and draw_history[i][item][0]>=540) or (draw_history[i][item][0]<=260 and draw_history[i][item][0]>=245):
+                        if (draw_history[i][item][1]<=255 and draw_history[i][item][1]>=240) or (draw_history[i][item][1]<=80 and draw_history[i][item][1]>=65):
+                            #
+                            
+                            if i==0:
+                                
+                                c="pink"
+                                pp1+=1
+                                cv2.circle(img, (int(draw_history[i][item][0]), int(draw_history[i][item][1])), 3, colors[c], 6)
+                            elif i==1:
+                                c="purple"
+                                pp2+=1
+                                cv2.circle(img, (int(draw_history[i][item][0]), int(draw_history[i][item][1])), 3, colors[c], 6)
+                        else:
+                            if i==0:
+                                c="red"
+                            elif i==1:
+                                c="blue"
+                            cv2.circle(img, (int(draw_history[i][item][0]), int(draw_history[i][item][1])), 3, colors[c], 6)
+                        
+
+                    else:
+                        if i==0:
+                            c="red"
+                        elif i==1:
+                            c="blue"
+                        cv2.circle(img, (int(draw_history[i][item][0]), int(draw_history[i][item][1])), 3, colors[c], 6)
+                     
+                        
             img = cv2.flip(img, 1)
             cv2.imshow('Results',img)
             cv2.waitKey(1)
             time.sleep(9)
             cv2.destroyAllWindows()
-            result=int(input("Your vote[1 or 2]: "))
-            
-           
-            #print(result)
-            if result==1:
+            if pp1>pp2:
                 print("Player one wins!")
                 wins[0]+=1
-            elif result==2:
+            elif pp2>pp1:
                 print("Player two wins!")
                 wins[1]+=1
+            elif pp1==pp2:
+                print("Draw!")
             
             print("Game results: ")
             print("Player 1 -",wins[0], "wins")
             print("Player 2 -",wins[1], "wins")
             cv2.destroyAllWindows()
-
             break
 
 
